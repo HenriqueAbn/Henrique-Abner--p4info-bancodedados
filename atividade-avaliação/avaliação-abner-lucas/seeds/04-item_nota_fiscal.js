043104c2219951162b7f94d0732d857408feaf0e
Henrique Abn <102832927+HenriqueAbn@users.noreply.github.com>
2022-05-23 23:19:25 -0300

    Add files via upload

---
+++ atividade-avaliação/avaliação-abner-lucas/seeds/04-item_nota_fiscal.js
@@ -0,0 +1,14 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('TB_ITEM_NOTA_FISCAL').del()
+  await knex('TB_ITEM_NOTA_FISCAL').insert([
+    {id: 1, sequencial: 'xx'},
+    {id: 2, quantidade: 'xx'},
+    {id: 3, produto: 'Produto'},
+    {id: 4, notaFiscal: 'xx'}
+  ]);
+};
